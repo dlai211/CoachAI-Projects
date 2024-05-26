314327709a9cb0e634fb50a82bfe240bfa43daab
dlai <72419337+dlai211@users.noreply.github.com>
2024-05-26 04:02:26 -0700

    Delete CoachAI-Challenge-IJCAI2023/Track 2: Stroke Forecasting directory

--- CoachAI-Challenge-IJCAI2023/Track 2: Stroke Forecasting/src/script.sh
+++
@@ -1,3 +0,0 @@
-current_time=`date +%Y%m%d-%H%M%S`
-model_path="./model/${current_time}_ShuttleNet/"
-python train.py --output_folder_name ${model_path} --model_type ShuttleNet --encode_length 4 --seed_value 42
